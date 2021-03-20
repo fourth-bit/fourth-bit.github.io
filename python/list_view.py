7d2b3dc55164ca92cbe6a0e87be92ae5e6bdf704
fourth-bit <ryanwolk@icloud.com>
2021-03-20 15:52:14 -0700

    Changed how script does output

--- python/list_view.py
+++ python/list_view.py
@@ -1,26 +1,11 @@
 import json
+import sys
+
+from . import util
 
 CHARITIES_JSON = './json/charities.json'
 LIST_TEMPLATE = './templates/list-template.html'
 ROOT_TEMPLATE = './templates/default.html'
-
-def subsitute_object(file, obj) -> str:
-    """Subsitute fields of dictionatry into a copy of a file.
-    This function will seek out all the keys in the form of {key}
-    within the file, then it will subsitute the value into them.
-    Returns a string, does not create nor edit files
-    """
-    template_file = open(file)
-    template = template_file.read()
-    template_file.close()
-
-    for field in obj.keys():
-        if template.find('{' + field + '}') == -1:
-            continue
-
-        # Strings are immutable
-        template = template.replace('{' + field + '}', obj[field]) 
-    return template
 
 def gen_list_view() -> str:
     """Generates list view of charities. Returns string
@@ -41,7 +26,7 @@ def gen_list_view() -> str:
     </div>""")
 
     for obj in charities:
-        temp = subsitute_object(LIST_TEMPLATE, obj)
+        temp = util.subsitute_object(LIST_TEMPLATE, obj)
         insert_index = template.find('{INSERT CHARITIES}')
         template = template[:insert_index] + temp + '\n' + template[insert_index:]
     template = template.replace('{INSERT CHARITIES}', '')
@@ -49,7 +34,11 @@ def gen_list_view() -> str:
     return template
 
 def main():
-    print(gen_list_view())
+    list_view = gen_list_view()
+    for x in sys.argv[1:]:
+        fp = open(x, 'w')
+        fp.write(list_view)
+        fp.close()
 
 if __name__ == '__main__':
     main()
